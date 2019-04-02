f757a48dfe090dcad6a7f602aa98aa5ca6231553
Alexender Perfiliev <perfiliev29@yandex.ru>
2019-04-02 22:19:58 +0300

    add structure to the progect

---
+++ src/components/header/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './style.css';
+
+
+class Header extends Component {
+
+    render() {
+
+        return (
+           <header>
+               test
+           </header>
+        );
+    };
+}
+
+
+export default Header;
